a4a0a420e678a3fbcfc5b2a98cfcf1942afeedbe
sjogrenjohan <sjogren_johan@hotmail.com>
2018-10-04 15:19:17 +0200

    rndm changes

--- javaS.js
+++ javaS.js
@@ -25,18 +25,41 @@ function randomBackgroundColor() {
     var blue = Math.floor(Math.random() * 256);
     var backgroundColor = "rgb(" + red + "," + green + "," + blue + ")"; 
 
+
     document.body.style.background = backgroundColor;
     
 }
 
 function hexColor() {
-    var red1 = Math.round(Math.random() * 9);
-    var red2 = Math.round(Math.random() * 9);
-    var green1 = Math.round(Math.random() * 9);
-    var green2 = Math.round(Math.random() * 9);
-    var blue1 = Math.round(Math.random() * 9);
-    var blue2 = Math.round(Math.random() * 9);
+    var red1 = convertToHex(Math.round(Math.random() * 15));
+    var red2 = convertToHex(Math.round(Math.random() * 15));
+    var green1 = convertToHex(Math.round(Math.random() * 15));
+    var green2 = convertToHex(Math.round(Math.random() * 15));
+    var blue1 = convertToHex(Math.round(Math.random() * 15));
+    var blue2 = convertToHex(Math.round(Math.random() * 15));
+
+
+
     var bghexColor = "#"+ red1 + "" + red2 + "" + green1 + "" + green2 + "" + blue1 + "" + blue2 +"";
+    console.log(bghexColor);
 
     document.body.style.background = bghexColor;
+}
+
+function convertToHex(value) {
+    if (value == 10) {
+        return "A";
+    } else if (value == 11) {
+        return "B";
+    } else if (value == 12) {
+        return "C";
+    } else if (value == 13) {
+        return "D";
+    } else if (value == 14) {
+        return "E";
+    } else if (value == 15) {
+        return "F";
+    } else {
+        return value;
+    }
 }
