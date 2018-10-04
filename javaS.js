635ddfb303236ceb2421e3724cb0bba58048c733
sjogrenjohan <sjogren_johan@hotmail.com>
2018-10-04 11:41:42 +0200

    removed console log

--- javaS.js
+++ javaS.js
@@ -24,7 +24,7 @@ function randomBackgroundColor() {
     var y = Math.floor(Math.random() * 256);
     var z = Math.floor(Math.random() * 256);
     var backgroundColor = "rgb(" + x + "," + y + "," + z + ")"; 
-   console.log(backgroundColor)
+
     document.body.style.background = backgroundColor;
     
 }
