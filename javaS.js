644e19e91774f778d00763d38c38df6e277a2455
sjogrenjohan <sjogren_johan@hotmail.com>
2018-10-04 14:26:55 +0200

    added random hex js

--- javaS.js
+++ javaS.js
@@ -20,11 +20,23 @@ function changeBackgroundColorBlack() {
 }
 
 function randomBackgroundColor() {
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    var backgroundColor = "rgb(" + x + "," + y + "," + z + ")"; 
+    var red = Math.floor(Math.random() * 256);
+    var green = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+    var backgroundColor = "rgb(" + red + "," + green + "," + blue + ")"; 
 
     document.body.style.background = backgroundColor;
     
+}
+
+function hexColor() {
+    var red1 = Math.round(Math.random() * 9);
+    var red2 = Math.round(Math.random() * 9);
+    var green1 = Math.round(Math.random() * 9);
+    var green2 = Math.round(Math.random() * 9);
+    var blue1 = Math.round(Math.random() * 9);
+    var blue2 = Math.round(Math.random() * 9);
+    var bghexColor = "#"+ red1 + "" + red2 + "" + green1 + "" + green2 + "" + blue1 + "" + blue2 +"";
+
+    document.body.style.background = bghexColor;
 }
