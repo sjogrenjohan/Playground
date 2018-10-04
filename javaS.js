4945365d5a7920a45b69ab810cb81530b3c01abc
sjogrenjohan <sjogren_johan@hotmail.com>
2018-10-04 11:40:26 +0200

    random changes to bg

--- javaS.js
+++ javaS.js
@@ -1,2 +1,30 @@
 
-console.log("It works!");
+
+
+function changeBackgroundColorRed() {
+    
+    
+    if (document.body.style.background == "white") {
+        document.body.style.background = "red";   
+    } else {
+        document.body.style.background = "white";
+    }
+
+
+
+}
+
+function changeBackgroundColorBlack() {
+    
+    document.body.style.background = "black";
+}
+
+function randomBackgroundColor() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var backgroundColor = "rgb(" + x + "," + y + "," + z + ")"; 
+   console.log(backgroundColor)
+    document.body.style.background = backgroundColor;
+    
+}
